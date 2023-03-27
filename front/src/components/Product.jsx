c0de3a6dd88b29b29d3ba9d4aa4c18f070bb4c33
EyaTriki <eya2000triki@gmail.com>
2023-03-27 14:48:18 +0100

    single product

--- front/src/components/Product.jsx
+++ front/src/components/Product.jsx
@@ -3,6 +3,7 @@ import {
     SearchOutlined,
     ShoppingCartOutlined,
   } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
   import styled from "styled-components";
   
@@ -76,9 +77,9 @@ import {
             <ShoppingCartOutlined />
           </Icon>
           <Icon>
-           
+            <Link to={`/product/${item._id}`}>
             <SearchOutlined />
-          
+          </Link> 
           </Icon>
           <Icon>
             <FavoriteBorderOutlined />
